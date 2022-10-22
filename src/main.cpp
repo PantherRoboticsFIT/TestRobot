8fbf4b60cb188b889aacca9792795f72138215d1
trainervolk7 <67025749+trainervolk7@users.noreply.github.com>
2022-10-22 16:15:37 -0400

    Chassis Controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "main.h"
-
+std::shared_ptr<ChassisController> drive =
+	ChassisControllerBuilder()
+		.withMotors({-11, -12}, {20, 19})
+		// Green gearset, 4 in wheel diam, 11.5 im wheel track
+		// 36 to 60 gear ratio
+		.withDimensions({AbstractMotor::gearset::blue},{{2.75_in, 12_in}, imev5BlueTPR})
+		.build();
 
 void on_center_button() {
 	
