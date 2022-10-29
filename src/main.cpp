5d676d47a9b86e4d655ec8bc7f1719ace027b44a
Michael Nicol <michaelnicol71@gmail.com>
2022-10-29 15:02:26 -0400

    Michael, Dan, Camid, 10-29-22

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,80 @@
 #include "main.h"
+#include "okapi/api/device/motor/abstractMotor.hpp"
+#define LEFT_WHEELS_PORT -11
+#define RIGHT_WHEELS_PORT -12
+#define HINGE_PORT 1
+#define ROLLER_PORT 15
 
 
-void on_center_button() {
-	
+std::shared_ptr<ChassisController> drive =
+    ChassisControllerBuilder()
+        .withMotors({-20, -19}, {11, 12})
+        // Green gearset, 4 in wheel diam, 11.5 im wheel track
+        // 36 to 60 gear ratio
+        .withDimensions({AbstractMotor::gearset::blue}, {{2.75_in, 12_in}, imev5BlueTPR})
+        .build();
+
+Motor hinge({HINGE_PORT});
+Motor roller({ROLLER_PORT});
+
+Controller controller;
+
+ControllerButton hingeButton1(ControllerDigital::L1);
+ControllerButton hingeButton2(ControllerDigital::R1);
+ControllerButton rollerButton(ControllerDigital::A);
+
+
+void on_center_button()
+{
+   // any time
 }
 
-
-void initialize() {
-	
+void initialize()
+{
+   hinge.setEncoderUnits(AbstractMotor::encoderUnits::degrees);
+   hinge.tarePosition();
+   roller.tarePosition();
 }
 
+void disabled()
+{
+   // signal to not start
+}
 
-void disabled() {}
+void competition_initialize()
+{
+   // I believe, I have got it written down somewhere let me bring that up
+}
 
+void autonomous()
+{
+   // On the switch that you connect your controller to, it has a automomous flip switch.
+}
 
-void competition_initialize() {}
+void opcontrol()
+{
+   while (true)
+   {
+      drive->getModel()->arcade(controller.getAnalog(ControllerAnalog::leftY),
+                                controller.getAnalog(ControllerAnalog::rightX));
+      
+      pros::delay(10);
+      if (hingeButton1.changedToPressed()) {
+         hinge.moveVelocity(-10);
+      }
+      if (hingeButton2.changedToPressed()) {
+         hinge.moveVelocity(10);
+      }
+      if (hingeButton1.changedToReleased() || hingeButton2.changedToReleased() 
+            || (hingeButton1.changedToPressed() && hingeButton2.changedToPressed())) {
+         hinge.moveVelocity(0);
+      }
 
-
-void autonomous() {}
-
-
-void opcontrol() {
-	while (true) {
-		
-	}
+      if (rollerButton.changedToPressed()) {
+         roller.moveVelocity(10);
+      }
+      if (rollerButton.changedToReleased()) {
+         roller.moveVelocity(0);
+      }
+   }
 }
