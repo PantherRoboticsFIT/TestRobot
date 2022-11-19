71783cdb350d2d206b7afd4169f5eb7a94d9adc3
trainervolk7 <67025749+trainervolk7@users.noreply.github.com>
2022-11-19 13:27:57 -0500

    updated proper chasis motors

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include "main.h"
 std::shared_ptr<ChassisController> drive =
 	ChassisControllerBuilder()
-		.withMotors({-11, -12}, {20, 19})
+		.withMotors({-20, -19}, {11, 12})
 		// Green gearset, 4 in wheel diam, 11.5 im wheel track
 		// 36 to 60 gear ratio
 		.withDimensions({AbstractMotor::gearset::blue},{{2.75_in, 12_in}, imev5BlueTPR})
 		.build();
 
+ControllerButton ArmButtonUp (ControllerDigital::L1);
+ControllerButton ArmButtonDown (ControllerDigital::R1);
+Motor arm {15};
 void on_center_button() {
 	
 }
@@ -28,6 +31,14 @@ void autonomous() {}
 
 void opcontrol() {
 	while (true) {
-		
+		if (ArmButtonUp.isPressed()){
+			arm.moveVelocity(-600);
+		}
+		else if (ArmButtonDown.isPressed()){
+			arm.moveVelocity(600);
+		}
+		else{
+			arm.moveVelocity(0);
+		}
 	}
 }
